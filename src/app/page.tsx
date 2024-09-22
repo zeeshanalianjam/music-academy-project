14de7b51505c50f0f5537e014f05804434aa2c3d
zeeshanali <zeeshanalianjam49@gmail.com>
2024-09-22 11:03:59 +0500

    almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -10,6 +13,9 @@ export default function Home() {
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <TestimonialCards/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
